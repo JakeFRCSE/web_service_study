0dc81b530f9c890bd88eec1570e39d4f003b50a5
Karryun <165464282+Karryun@users.noreply.github.com>
2024-11-08 15:41:34 +0900

    feat: ui step1

--- ai_web_front/app/page.js
+++ ai_web_front/app/page.js
@@ -1,95 +1,62 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
+import React, { useEffect, useState } from 'react';
+import styles from './page.module.css';
+
+const LandingPage = () => {
+  const tempData = `{
+	"reviews": [
+			{
+				"id": 423,
+				"reviewContents": "dskafnsdklafnksdalf",
+				"modelRatings": 3
+			},
+			{
+				"id": 423,
+				"reviewContents": "dskafnsdklafnksdalf",
+				"modelRatings": 3
+			},
+			{
+				"id": 423,
+				"reviewContents": "dskafnsdklafnksdalf",
+				"modelRatings": 3
+			},
+			{
+				"id": 423,
+				"reviewContents": "dskafnsdklafnksdalf",
+				"modelRatings": 3
+			}
+		],
+	"pageinfo": {
+		"maxPage": 5
+	}
+}`
+
+  const [data, setData] = useState(JSON.parse(tempData));
+
+  useEffect(()=>{
+    console.log(data);
+  }, []);
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={styles.Landing}>
+      <div className={styles.Container}>
+      <div className={styles.navigationBar}>
+        <div className={styles.navItem1}>AI Developer</div>
+        <div className={styles.navItem2}>맥도날드 리뷰별점 예측모델</div>
+      </div>
+        <div className={styles.semi_content}>가장 최근에 다녀왔던 맥도날드의 리뷰를 영어로 남겨보세요!</div>
+        <div className={styles.semi_content_2}> AI가 당신이 남길 별점을 예측해드립니다.</div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className={styles.all_chatting_box}></div>
+
+      <div className={styles.inputContainer}> 
+        <input className={styles.input} type="text" />
+        <button className={styles.Upload}></button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
-}
+};
+
+export default LandingPage;
